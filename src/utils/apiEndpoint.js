3127156630819c8e63d5d91afd48194b033e37bf
Jimmy <c.jimmy.tw@gmail.com>
2023-04-23 18:14:01 +0800

    Fix/proxy issue

--- src/utils/apiEndpoint.js
+++ src/utils/apiEndpoint.js
@@ -1,3 +1,4 @@
-export const EXPENDITURES_API = "/api/v1/expenditures";
-export const TASKS_API = "/api/v1/tasks";
-export const PEOPLEMEMO_API = "/api/v1/peopleMemos";
+let apiUrl = process.env.API_URL || "http://localhost:4000";
+export const EXPENDITURES_API = apiUrl + "/api/v1/expenditures";
+export const TASKS_API = apiUrl + "/api/v1/tasks";
+export const PEOPLEMEMO_API = apiUrl + "/api/v1/peopleMemos";
